ae8c9bc5d2186390146c198e7ee07d86c966e71e
salimus06 <148533821+salimus06@users.noreply.github.com>
2024-03-28 10:25:16 +0100

    fix : change redis host

--- test/configure.js
+++ test/configure.js
@@ -4,11 +4,11 @@ const configure = require('../src/configure')
 describe('Configure', () => {
   it('load default json configuration file', () => {
     const config = configure()
-    expect(config.redis).to.eql({"host": "127.0.0.1", "port": 6379})
+    expect(config.redis).to.eql({"host": "redis", "port": 6379})
   })
   it('load custom configuration', () => {
     const config_custom = {"custom": "value"}
     const config = configure(config_custom)
-    expect(config).to.eql({"redis": {"host": "127.0.0.1", "port": 6379}, "custom": "value"})
+    expect(config).to.eql({"redis": {"host": "redis", "port": 6379}, "custom": "value"})
   })
 })
